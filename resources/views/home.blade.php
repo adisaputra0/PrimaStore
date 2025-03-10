39270e47fd3d37353d96bb87e787dd12aee02616
I Putu Adi Saputra <putuadi208@gmail.com>
2025-03-10 19:08:02 +0800

    add tailwind

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,8 +6,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
+    @vite('resources/css/app.css')
 </head>
 <body>
-    <h1>Welcome to Home Page!</h1>
+    <h1 class=" text-red-500">Welcome to Home Page!</h1>
 </body>
 </html>
